7ea2e086fb08baba56e676daa8278a3a52e3f207
IsraelM17 <se.israel.mc@gmail.com>
2019-12-21 21:26:36 -0600

    Se creo archivo para las preferencias de usuario

---
+++ lib/src/preferences/userPreferences.dart
@@ -0,0 +1,32 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences{
+
+  static final UserPreferences _instancePreferences = UserPreferences.internal();
+
+
+  factory UserPreferences(){
+    return _instancePreferences;
+  }
+
+  UserPreferences.internal();
+
+  SharedPreferences _preferences;
+
+  initPreferences() async {
+    this._preferences = await SharedPreferences.getInstance();
+  }
+
+  clearUserPreferences(){
+    _preferences.clear();
+  }
+
+  get session{
+    return _preferences.getBool('session');
+  }
+
+  set session(bool session){
+    _preferences.setBool('session', session);
+  }
+
+}
